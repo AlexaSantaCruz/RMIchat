e5c5ff91623f1b96a6f0c7b29bcea6fd824fa252
alex <92236669+AlexaSantaCruz@users.noreply.github.com>
2023-11-24 09:19:15 -0600

    Conexiones por visual

--- src/rmi/ChatClient.java
+++ src/rmi/ChatClient.java
@@ -5,19 +5,19 @@
 package rmi;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
 public class ChatClient {
+    ChatService chatService;
     
-    
-    public void startClient(){
+    public void startClient(String username){
         try {
-            String username = "prueba3";
             //String serverUrl = "rmi://192.168.84.107:9000/ChatService";
             String serverUrl = "rmi://localhost:9000/ChatService";
 
             
-            ChatService chatService = (ChatService) Naming.lookup(serverUrl);
+            chatService = (ChatService) Naming.lookup(serverUrl);
 
             ClientCallbackImpl clientCallback = new ClientCallbackImpl(username);
             chatService.registerClient(clientCallback, username);
@@ -25,27 +25,38 @@ public class ChatClient {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Ingrese 'salir' para salir del chat.");
 
-            while (true) {
-                String message = scanner.nextLine();
-                if (message.equals("salir")) {
-                    chatService.unregisterClient(clientCallback);
-                    System.exit(0);
-                } else {
-                    if (message.startsWith("/privado")) {
-                        String[] parts = message.split(" ", 3);
-                        if (parts.length == 3) {
-                            chatService.sendDirectMessage(username, parts[1], parts[2]);
-                        } else {
-                            System.out.println("Formato incorrecto. Uso: /privado <usuario> <mensaje>");
-                        }
-                    } else {
-                        chatService.broadcastMessage(username + ": " + message);
-                    }
-                }
-            }
+//            while (true) {
+//                String message = scanner.nextLine();
+//                if (message.equals("salir")) {
+//                    chatService.unregisterClient(clientCallback);
+//                    System.exit(0);
+//                } else {
+//                    if (message.startsWith("/privado")) {
+//                        String[] parts = message.split(" ", 3);
+//                        if (parts.length == 3) {
+//                            chatService.sendDirectMessage(username, parts[1], parts[2]);
+//                        } else {
+//                            System.out.println("Formato incorrecto. Uso: /privado <usuario> <mensaje>");
+//                        }
+//                    } else {
+//                        chatService.broadcastMessage(username + ": " + message);
+//                    }
+//                }
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public void sendMessage(String message, String username) throws RemoteException{
+        chatService.broadcastMessage(username + ": " + message);
+
+    }
+    
+    public void sendPrivate(String username, String privateuser, String message) throws RemoteException{
+        chatService.sendDirectMessage(username, privateuser, message);
+        
+
     }
     
     public static void main(String[] args) {
