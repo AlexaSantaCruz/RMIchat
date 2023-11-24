e3b570e1e18d792771bb73ca6fbf9760a1ef1bf5
alex <92236669+AlexaSantaCruz@users.noreply.github.com>
2023-11-23 23:34:09 -0600

    Mensajes privados(consola)

--- src/rmi/ChatClient.java
+++ src/rmi/ChatClient.java
@@ -8,13 +8,55 @@ import java.rmi.Naming;
 import java.util.Scanner;
 
 public class ChatClient {
-    public static void main(String[] args) {
+    
+    
+    public void startClient(){
         try {
-            String username = "prueba2";
-            String serverUrl = "rmi://localhost/ChatService";
+            String username = "prueba3";
+            //String serverUrl = "rmi://192.168.84.107:9000/ChatService";
+            String serverUrl = "rmi://localhost:9000/ChatService";
+
+            
             ChatService chatService = (ChatService) Naming.lookup(serverUrl);
 
-            ClientCallbackImpl clientCallback = new ClientCallbackImpl();
+            ClientCallbackImpl clientCallback = new ClientCallbackImpl(username);
+            chatService.registerClient(clientCallback, username);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese 'salir' para salir del chat.");
+
+            while (true) {
+                String message = scanner.nextLine();
+                if (message.equals("salir")) {
+                    chatService.unregisterClient(clientCallback);
+                    System.exit(0);
+                } else {
+                    if (message.startsWith("/privado")) {
+                        String[] parts = message.split(" ", 3);
+                        if (parts.length == 3) {
+                            chatService.sendDirectMessage(username, parts[1], parts[2]);
+                        } else {
+                            System.out.println("Formato incorrecto. Uso: /privado <usuario> <mensaje>");
+                        }
+                    } else {
+                        chatService.broadcastMessage(username + ": " + message);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        try {
+            String username = "prueba3";
+           //String serverUrl = "rmi://192.168.84.107:9000/ChatService";
+            String serverUrl = "rmi://localhost:9000/ChatService";
+            
+            ChatService chatService = (ChatService) Naming.lookup(serverUrl);
+
+            ClientCallbackImpl clientCallback = new ClientCallbackImpl(username);
             chatService.registerClient(clientCallback, username);
 
             Scanner scanner = new Scanner(System.in);
